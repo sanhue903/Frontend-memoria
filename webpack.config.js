067c66da001f504c12999a0bd6d5060a78a87c7e
Sebastián Sanhueza <s.sanhueza1001@gmail.com>
2024-06-24 16:11:22 -0400

    a

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    compress: true,
+    port: 8080,
+    allowedHosts: [
+      'goldfish-app-mahk3.ondigitalocean.app', // Agrega tu dominio aquí
+      '.ondigitalocean.app' // Permite subdominios en DigitalOcean
+    ],
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
